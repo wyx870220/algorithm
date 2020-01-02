3666eba9d378eaab36c4e37c28908d91034f5bca
wyx870220 <1710150013@qq.com>
2020-01-02 13:58:23 +0800

    0003

---
+++ algorithm/src/main/java/com/daxin/一一实际遇到的问题.java
@@ -0,0 +1,12 @@
+package com.daxin;
+
+/**
+ * Created by Administrator on 2020/1/2 0002.
+ */
+public class 一一实际遇到的问题 {
+    public static void main(String[] args) {
+        String str ="java   java  ,hello  world";
+        String string =str.replaceAll("java","王思鑫");
+        System.out.println(string);
+    }
+}
